4306c33fb078daf4c0d44a32ace4016a96f90bfc
sordid-rectangles <98424209+sordid-rectangles@users.noreply.github.com>
2022-01-28 08:46:20 -0500

    got rid of x, and mux. gross way to do that, going to use a self contained lib

--- main.go
+++ main.go
@@ -44,6 +44,10 @@ func main() {
 	//Declarations
 	var err error
 
+	Session.AddHandler(messageCreate)
+	// In this example, we only care about receiving message events.
+	Session.Identify.Intents = discordgo.IntentsGuildMessages
+
 	// Open a websocket connection to Discord
 	err = Session.Open()
 	if err != nil {
@@ -62,5 +66,23 @@ func main() {
 
 	// Exit Normally.
 	//exit
+
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 
 }
